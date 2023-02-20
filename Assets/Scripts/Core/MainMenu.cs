5c1e63e107a52553391bc217a0c41f74016169c9
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-02-21 05:25:14 +1100

    updated

--- Assets/Scripts/Core/MainMenu.cs
+++ Assets/Scripts/Core/MainMenu.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 namespace Minigame.Games.Core
 {
+    [RequireComponent(typeof(AudioSource))]
     public class MainMenu : MonoBehaviour
     {
         [SerializeField] private ClickFlash _clickFlash;
+        [SerializeField] private AudioClip _flashSfx;
         private void Update()
         {
             if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
             {
+                GetComponent<AudioSource>().PlayOneShot(_flashSfx);
                 _clickFlash.Enter();
             }
         }
