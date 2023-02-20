67160d820703ebafedf55c41b1bc1aeb60d673b5
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-02-21 05:47:19 +1100

    asdfv

--- Assets/Scripts/Minigame/MinigameManager.cs
+++ Assets/Scripts/Minigame/MinigameManager.cs
@@ -124,7 +124,7 @@ namespace Minigame
 
         private void TryLoadRequested(GameLayout gameLayout, Minigame minigame)
         {
-            if(minigame != null)
+            if(minigame != null && !_gameCompleted[minigame.Prefab])
                 LoadGame(minigame, gameLayout);
         }
 
