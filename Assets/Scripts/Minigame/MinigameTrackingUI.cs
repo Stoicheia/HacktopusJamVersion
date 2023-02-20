0f87a57bfc39f81d3a30506c2f373835ffdd835c
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-02-20 14:59:20 +1100

    fa

--- Assets/Scripts/Minigame/MinigameTrackingUI.cs
+++ Assets/Scripts/Minigame/MinigameTrackingUI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Minigame
 {
@@ -10,11 +11,15 @@ namespace Minigame
 
         [Header("Graphics")] [SerializeField] private TextMeshProUGUI _timerField;
         [SerializeField] private TextMeshProUGUI _progressField;
+        [SerializeField] private TextMeshProUGUI _progressPercentField;
+        [SerializeField] private Image _progressImage;
 
         private void Update()
         {
             _timerField.text = $"{_manager.Timer / 60:00}:{_manager.Timer % 60:00}";
             _progressField.text = $"{_manager.MinigamesCompleted}/{_manager.MinigamesCount}";
+            _progressPercentField.text = $"{(float)_manager.MinigamesCompleted/_manager.MinigamesCount:0%}%";
+            _progressImage.fillAmount = (float) _manager.MinigamesCompleted / _manager.MinigamesCount;
         }
     }
 }
