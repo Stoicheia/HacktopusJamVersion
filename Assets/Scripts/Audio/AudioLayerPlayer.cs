f1f83cfb64d91c9d4406b7198a19e59dffecd725
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-02-20 20:08:58 +1100

    audio foundations

---
+++ Assets/Scripts/Audio/AudioLayerPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Minigame.Games.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioLayerPlayer : MonoBehaviour
+    {
+        private AudioSource _audio;
+        private float _targetVolume;
+        [SerializeField] [Range(0.3f, 30)] private float _fadeStrength;
+        [SerializeField] private MusicLayerName _name;
+
+        public MusicLayerName Name => _name;
+
+        public float Volume
+        {
+            get => _audio.volume;
+            set => _targetVolume = value;
+        }
+
+        private void Awake()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            _audio.volume = Mathf.Lerp(_audio.volume, _targetVolume, _fadeStrength * Time.deltaTime);
+        }
+
+        public void Play(bool withVolume)
+        {
+            _audio.Play();
+            if(withVolume) Unmute();
+            else Mute();
+        }
+
+        public void Mute()
+        {
+            Volume = 0;
+        }
+
+        public void Unmute()
+        {
+            Volume = 1;
+        }
+}
+}
---
+++ Assets/Scripts/Audio/AudioLayersManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minigame.Games.Audio
+{
+    public enum MusicLayerName
+    {
+        
+    }
+    
+    public class AudioLayersManager : MonoBehaviour
+    {
+        [SerializeField] private MinigameManager _game;
+        [SerializeField] private List<AudioLayerPlayer> _layers;
+
+        private Dictionary<MusicLayerName, AudioLayerPlayer> _nameToLayer;
+
+        private void Awake()
+        {
+            _nameToLayer = new Dictionary<MusicLayerName, AudioLayerPlayer>();
+            foreach (var l in _layers)
+            {
+                _nameToLayer[l.Name] = l;
+            }
+        }
+    }
+}
